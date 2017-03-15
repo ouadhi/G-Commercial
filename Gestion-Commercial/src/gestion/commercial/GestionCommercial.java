e7a62c546ea35c9face99440ca68cc3d217d6f9e
cherabrab <mcher@DESKTOP-9317KUR>
2017-03-15 13:24:36 +0100

    Inovice class

--- Gestion-Commercial/src/gestion/commercial/GestionCommercial.java
+++ Gestion-Commercial/src/gestion/commercial/GestionCommercial.java
@@ -5,8 +5,8 @@
  */
 package gestion.commercial;
 
-import com.gestionCommerciale.HibernateSchema.Client;
-import com.gestionCommerciale.Models.ClientQueries;
+import com.gestionCommerciale.HibernateSchema.*;
+import com.gestionCommerciale.Models.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -49,13 +49,25 @@ public class GestionCommercial extends Application {
     public static void main(String[] args) {
         //launch(args);
         //test hibernate
-        ClientQueries clQueries= new ClientQueries();
-        Client clientObject= new Client("attman","3riwat");
+        ClientQueries clQueries = new ClientQueries();
+        Client clientObject = new Client("attman", "3riwat");
         clQueries.insererOuModifieClient(clientObject);
-        for (int i = 0; i <clQueries.clientsList().size() ; i++) {
-               System.out.println(clQueries.clientsList().get(i).getName() +" "+
-                       clQueries.clientsList().get(i).getPrenom());
+        for (int i = 0; i < clQueries.clientsList().size(); i++) {
+            System.out.println(clQueries.clientsList().get(i).getName() + " "
+                    + clQueries.clientsList().get(i).getPrenom());
         }
-    }
 
+        ProductQueries proQueries = new ProductQueries();
+        Product productObject = new Product("p1", 22, 2222.22f);
+        proQueries.SaveOrUpdateProduct(productObject);
+        productObject = new Product("p2", 33, 3333.33f);
+        proQueries.SaveOrUpdateProduct(productObject);
+        for (int i = 0; i < clQueries.clientsList().size(); i++) {
+            System.out.println(proQueries.productsList().get(i).getName() + ","
+                    + proQueries.productsList().get(i).getQuantity()+ ","
+                    + proQueries.productsList().get(i).getPrice());
+
+        }
+
+    }
 }
