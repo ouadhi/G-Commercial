62a0434d264289afc7f856db7249f79b31b749a2
mac <mac@192.168.1.100>
2017-07-02 16:59:01 +0100

    Focus

--- Gestion-Commercial/src/CommercialeControles/Client/AjouterClientController.java
+++ Gestion-Commercial/src/CommercialeControles/Client/AjouterClientController.java
@@ -18,6 +18,7 @@ import com.jfoenix.controls.JFXTextField;
 import UIControle.Methode;
 import UIControle.Notification;
 import UIControle.ShowPane;
+import com.jfoenix.controls.JFXListView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -32,122 +33,150 @@ import tray.notification.NotificationType;
 
 public class AjouterClientController implements Initializable {
 
-	@FXML
-	private JFXTextField nomtxt;
-	@FXML
-	private JFXTextField prenomtxt;
-	@FXML
-	private JFXTextField NRtxt;
-	@FXML
-	private JFXTextField NAtxt;
-	@FXML
-	private JFXTextField adressetxt;
-	@FXML
-	private JFXTextField activitetxt;
-	@FXML
-	private JFXTextField NCarteF;
-	@FXML
-	private DatePicker datedept;
-	@FXML
-	private JFXButton annuler;
-	@FXML
-	private Label savelabel;
-	@FXML
-	private ImageView close;
-	private JFXTextField Solde;
-	@FXML
-	private JFXComboBox<String> activiteBox;
-	@FXML
-	private JFXCheckBox fermer;
-	@FXML
-	private JFXTextField solde;
+    @FXML
+    private JFXTextField nomtxt;
+    @FXML
+    private JFXTextField prenomtxt;
+    @FXML
+    private JFXTextField NRtxt;
+    @FXML
+    private JFXTextField NAtxt;
+    @FXML
+    private JFXTextField adressetxt;
+    @FXML
+    private JFXTextField activitetxt;
+    @FXML
+    private JFXTextField NCarteF;
+    @FXML
+    private DatePicker datedept;
+    @FXML
+    private JFXButton annuler;
+    @FXML
+    private Label savelabel;
+    @FXML
+    private ImageView close;
+    private JFXTextField Solde;
+    @FXML
+    private JFXComboBox<String> activiteBox;
+    @FXML
+    private JFXCheckBox fermer;
+    @FXML
+    private JFXTextField solde;
 
-	private void clear() {
-		nomtxt.setText("");
-		prenomtxt.setText("");
-		NRtxt.setText("");
-		NAtxt.setText("");
-		adressetxt.setText("");
-		NCarteF.setText("");
-	}
+    public JFXListView<ClienCell> clientLsit = null;
 
-	@FXML
-	private void close(MouseEvent event) {
-		Stage currentSatge = Methode.getStageMouses(event);
-		currentSatge.close();
-	}
+    private void clear() {
+        nomtxt.setText("");
+        prenomtxt.setText("");
+        NRtxt.setText("");
+        NAtxt.setText("");
+        adressetxt.setText("");
+        NCarteF.setText("");
+    }
 
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		datedept.setValue(LocalDate.now());
-		Methode.SetUpper(nomtxt, 30);
-		Methode.setsizeString(prenomtxt, 30);
-		Methode.setsizeString(prenomtxt, 30);
-		Methode.SetUpper(NRtxt, 30);
-		Methode.SetUpper(NAtxt, 20);
-		Methode.SetUpper(NCarteF, 20);
+    @FXML
+    private void close(MouseEvent event) {
+        Stage currentSatge = Methode.getStageMouses(event);
+        currentSatge.close();
+    }
 
-		setActivty();
-	}
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        datedept.setValue(LocalDate.now());
+        Methode.SetUpper(nomtxt, 30);
+        Methode.setsizeString(prenomtxt, 30);
+        Methode.setsizeString(prenomtxt, 30);
+        Methode.SetUpper(NRtxt, 30);
+        Methode.SetUpper(NAtxt, 20);
+        Methode.SetUpper(NCarteF, 20);
 
-	@FXML
-	private void quitter(ActionEvent event) {
-		Stage currentSatge = Methode.getStage(event);
-		currentSatge.close();
-	}
+        setActivty();
+    }
 
-	@FXML
-	private void Sauvgarder(ActionEvent event) {
+    @FXML
+    private void quitter(ActionEvent event) {
+        Stage currentSatge = Methode.getStage(event);
+        currentSatge.close();
+        if (clientLsit == null) {
+            new ShowPane().showClient();
+        } else {
+            refreshListe();
+        }
+    }
 
-		String nom = nomtxt.getText();
-		String prenom = prenomtxt.getText();
-		String NR = NRtxt.getText();
-		String NA = NAtxt.getText();
-		String adresse = adressetxt.getText();
-		// String activite = activitetxt.getText();
-		String activite = activiteBox.getSelectionModel().getSelectedItem();
-		String Ncarte = NCarteF.getText();
+    @FXML
+    private void Sauvgarder(ActionEvent event) {
 
-		if (nom.isEmpty() || prenom.isEmpty() || NR.isEmpty() || NA.isEmpty() || adresse.isEmpty() || activite.isEmpty()
-				|| Ncarte.isEmpty() || datedept.getValue() == null) {
-			Notification.notif(NotificationType.ERROR, "Vérification", "Vérifier que tout les champs sont remplis!");
-		} else {
+        String nom = nomtxt.getText();
+        String prenom = prenomtxt.getText();
+        String NR = NRtxt.getText();
+        String NA = NAtxt.getText();
+        String adresse = adressetxt.getText();
+        // String activite = activitetxt.getText();
+        String activite = activiteBox.getSelectionModel().getSelectedItem();
+        String Ncarte = NCarteF.getText();
 
-			Date dateDepotDossier = Date.from(datedept.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        if (nom.isEmpty() || prenom.isEmpty() || NR.isEmpty() || NA.isEmpty() || adresse.isEmpty() || activite.isEmpty()
+                || Ncarte.isEmpty() || datedept.getValue() == null) {
+            Notification.notif(NotificationType.ERROR, "Vérification", "Vérifier que tout les champs sont remplis!");
+        } else {
 
-			if (ClientQueries.getClientByRegistre(NR) != null) {
-				// notification for already exists
-				Notification.error("Ce client est exite déja!");
-			} else {
-				// add to database
-				try {
-					Client client = new Client(nom, prenom, NR, NA, adresse, activite, dateDepotDossier, Ncarte);
-					ClientQueries.SaveOrUpdate(client);
-					Notification.Addnotification();
-					new ShowPane().showClient();
-					if (!fermer.isSelected()) {
-						quitter(event);
-					} else {
-						clear();
-					}
+            Date dateDepotDossier = Date.from(datedept.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant());
 
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
-			}
-		}
-	}
+            if (ClientQueries.getClientByRegistre(NR) != null) {
+                // notification for already exists
+                Notification.error("Ce client est exite déja!");
+            } else {
+                // add to database
+                try {
+                    Client client = new Client(nom, prenom, NR, NA, adresse, activite, dateDepotDossier, Ncarte);
+                    ClientQueries.SaveOrUpdate(client);
+                    Notification.Addnotification();
 
-	public void setActivty() {
-		List<String> listeActivty = new ArrayList<>();
-		listeActivty.add("Boulangerie");
-		listeActivty.add("Agriculteur");
-		listeActivty.add("Grossiste");
-		listeActivty.add("Autre");
+                    if (!fermer.isSelected()) {
+                        if (clientLsit == null) {
+                            new ShowPane().showClient();
+                        } else {
+                            refreshListe();
+                        }
+                        quitter(event);
 
-		ObservableList<String> list = FXCollections.observableList(listeActivty);
+                    } else {
+                        clear();
+                    }
 
-		activiteBox.setItems(list);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 
-	}
+    public void setActivty() {
+        List<String> listeActivty = new ArrayList<>();
+        listeActivty.add("Boulangerie");
+        listeActivty.add("Agriculteur");
+        listeActivty.add("Grossiste");
+        listeActivty.add("Autre");
+
+        ObservableList<String> list = FXCollections.observableList(listeActivty);
+
+        activiteBox.setItems(list);
+
+    }
+
+    private void refreshListe() {
+        List<Client> listClientsDB = ClientQueries.list();
+        List<ClienCell> list = new ArrayList<>();
+        for (int i = 0; i < listClientsDB.size(); i++) {
+            list.add(new ClienCell(listClientsDB.get(i)));
+        }
+        ObservableList<ClienCell> myObservableList = FXCollections.observableList(list);
+        clientLsit.setItems(myObservableList);
+        clientLsit.setExpanded(true);
+    }
+
+    public void setData(JFXListView<ClienCell> clientLsit) {
+        this.clientLsit = clientLsit;
+    }
 }
